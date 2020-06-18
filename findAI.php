7d738d1c5c537ccb7281408dc4af28b70b9c2341
sidarthrajan <srajan39@gatech.edu>
2020-06-17 23:54:52 -0500

    Finished search page

--- findAI.php
+++ findAI.php
@@ -396,105 +396,135 @@
                 <div class="row">
 
                     <!-- Area Chart -->
-                    <div class="col-xl-8 col-lg-7">
+                    <div class="col-xl-12 col-lg-12">
                         <div class="card shadow mb-4">
                             <!-- Card Header - Dropdown -->
                             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 class="m-0 font-weight-bold text-primary prim-text">Earnings Overview</h6>
+                                <h6 class="m-0 font-weight-bold text-primary prim-text">Top Choices</h6>
                                 <div class="dropdown no-arrow">
                                     <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                                     </a>
                                     <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                                        <div class="dropdown-header">Graph Specifics</div>
-                                        <a class="dropdown-item" href="#">Monthly</a>
-                                        <a class="dropdown-item" href="#">Daily</a>
+                                        <div class="dropdown-header">Menu Options</div>
+                                        <a class="dropdown-item" href="#">Show All</a>
+                                        <a class="dropdown-item" href="#">Filter</a>
                                     </div>
                                 </div>
                             </div>
                             <!-- Card Body -->
                             <div class="card-body">
-                                <div class="chart-area">
-                                    <canvas id="myAreaChart"></canvas>
+                                <div class="row">
+                                    <div class="col-xl-4 col-md-6 mb-4">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <a style="color: limegreen" class="nav-link" href="#" id="sortDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-user fa-sm"></i>
+                                                    <span>John Doe</span>
+                                                    <ul>
+                                                        <li>Expertise: </li>
+                                                        <li>Value: </li>
+                                                        <li>Network: </li>
+                                                    </ul>
+                                                    <i class="fas fa-mail-bulk fa-lg"></i>
+                                                    <span>Click to Connect</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 col-md-6 mb-4">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <a style="color: limegreen" class="nav-link" href="#" id="sortDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-user fa-sm"></i>
+                                                    <span>John Doe</span>
+                                                    <ul>
+                                                        <li>Expertise: </li>
+                                                        <li>Value: </li>
+                                                        <li>Network: </li>
+                                                    </ul>
+                                                    <i class="fas fa-mail-bulk fa-lg"></i>
+                                                    <span>Click to Connect</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 col-md-6 mb-4">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <a style="color: limegreen" class="nav-link" href="#" id="sortDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-user fa-sm"></i>
+                                                    <span>John Doe</span>
+                                                    <ul>
+                                                        <li>Expertise: </li>
+                                                        <li>Value: </li>
+                                                        <li>Network: </li>
+                                                    </ul>
+                                                    <i class="fas fa-mail-bulk fa-lg"></i>
+                                                    <span>Click to Connect</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class ="row">
+                                    <div class="col-xl-4 col-md-6 mb-4">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <a style="color: limegreen" class="nav-link" href="#" id="sortDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-user fa-sm"></i>
+                                                    <span>John Doe</span>
+                                                    <ul>
+                                                        <li>Expertise: </li>
+                                                        <li>Value: </li>
+                                                        <li>Network: </li>
+                                                    </ul>
+                                                    <i class="fas fa-mail-bulk fa-lg"></i>
+                                                    <span>Click to Connect</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 col-md-6 mb-4">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <a style="color: limegreen" class="nav-link" href="#" id="sortDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-user fa-sm"></i>
+                                                    <span>John Doe</span>
+                                                    <ul>
+                                                        <li>Expertise: </li>
+                                                        <li>Value: </li>
+                                                        <li>Network: </li>
+                                                    </ul>
+                                                    <i class="fas fa-mail-bulk fa-lg"></i>
+                                                    <span>Click to Connect</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 col-md-6 mb-4">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <a style="color: limegreen" class="nav-link" href="#" id="sortDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-user fa-sm"></i>
+                                                    <span>John Doe</span>
+                                                    <ul>
+                                                        <li>Expertise: </li>
+                                                        <li>Value: </li>
+                                                        <li>Network: </li>
+                                                    </ul>
+                                                    <i class="fas fa-mail-bulk fa-lg"></i>
+                                                    <span>Click to Connect</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <!-- Pie Chart -->
-                    <div class="col-xl-4 col-lg-5">
-                        <div class="card shadow mb-4">
-                            <!-- Card Header - Dropdown -->
-                            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 class="m-0 font-weight-bold text-primary">Equity Owned</h6>
-                            </div>
-                            <!-- Card Body -->
-                            <div class="card-body">
-                                <div class="chart-pie pt-4 pb-2">
-                                    <canvas id="myPieChart"></canvas>
-                                </div>
-                                <div class="mt-4 text-center small">
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-blue"></i> Company
-                    </span>
-                                    <span class="mr-2">
-                      <i class="fas fa-circle text-success"></i> Investors
-                    </span>
-                                    <span class="mr-2">
-                      <i class="fas fa-circle text-info"></i> Other
-                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
-                <!-- Content Row -->
-                <div class="row">
-
-                    <!-- Content Column -->
-                    <div class="col-lg-6 mb-4">
-
-                        <!-- Bar Chart -->
-                        <div class="card shadow mb-4">
-                            <div class="card-header py-3">
-                                <h6 class="m-0 font-weight-bold text-primary">Spending</h6>
-                            </div>
-                            <div class="card-body">
-                                <div class="chart-bar">
-                                    <canvas id="myBarChart"></canvas>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <!-- Pie Chart -->
-                    <div class="col-lg-6 mb-4">
-                        <div class="card shadow mb-4">
-                            <!-- Card Header - Dropdown -->
-                            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 class="m-0 font-weight-bold text-primary">Spending Percentage</h6>
-                            </div>
-                            <!-- Card Body -->
-                            <div class="card-body">
-                                <div class="chart-pie pt-4 pb-2">
-                                    <canvas id="myPieChart2"></canvas>
-                                </div>
-                                <div class="mt-4 text-center small">
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-blue"></i> Employees
-                    </span>
-                                    <span class="mr-2">
-                      <i class="fas fa-circle text-success"></i> Materials
-                    </span>
-                                    <span class="mr-2">
-                      <i class="fas fa-circle text-info"></i> Waste
-                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
 
                 </div>

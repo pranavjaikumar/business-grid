8dfea622a9a0a15824985ccef6a6801bb5009a96
sidarthrajan <srajan39@gatech.edu>
2020-06-19 13:57:41 -0500

    Did Login and Registration

--- login.php
+++ login.php
@@ -1,1 +1,25 @@
 <?php
+include('config.php');
+session_start();
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    //$password_hashed = password_hash($password, PASSWORD_BCRYPT);
+
+    $query = mysqli_query($link, "SELECT * FROM users WHERE email = '$email' and password = '$password'");
+    $row = mysqli_fetch_array($query);
+    if ($row['email'] == $email && $row['password'] == $password) {
+        $_SESSION["id"] = $row['id'];
+        if ($row['type'] == "Startup") {
+            header("Location: startupDashboard.php");
+        } else if ($row['type'] == "Business Professional") {
+            header("Location: bpDashboard.php");
+        } else {
+            header("Location: aiDashboard.php");
+        }
+    } else {
+        $_SESSION["id"] = $row['id'];
+        header('Location: index.php');
+    }
+
+?>
---
+++ make.php
@@ -0,0 +1,38 @@
+<?php
+
+include('config.php');
+session_start();
+
+if (isset($_POST['Register'])) {
+
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm = $_POST['confirm'];
+    $type = $_POST['type'];
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+    $query = $link->prepare("SELECT * FROM users WHERE EMAIL=:email");
+    $query->bindParam("email", $email, PDO::PARAM_STR);
+    $query->execute();
+
+    if ($query->rowCount() > 0) {
+        echo '<p class="error">The email address is already registered!</p>';
+    }
+
+    if ($query->rowCount() == 0) {
+        $query = $link->prepare("INSERT INTO users (first_name, last_name, email, password, type) values ('$first_name', '$last_name', '$email', '$password', '$type')");
+        $query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
+        $query->bindParam("email", $email, PDO::PARAM_STR);
+        $result = $query->execute();
+
+        if ($result) {
+            echo '<p class="success">Your registration was successful!</p>';
+        } else {
+            echo '<p class="error">Something went wrong!</p>';
+        }
+    }
+}
+
+?>

10fc927b580c79bc634837b5f763ebe90561216f
pranavjaikumar <jaikumar@purdue.edu>
2020-06-20 12:13:36 -0500

    Showing the user's name on top right of dashboard

--- login.php
+++ login.php
@@ -10,6 +10,8 @@ session_start();
     $row = mysqli_fetch_array($query);
     if ($row['email'] == $email && $row['password'] == $password) {
         $_SESSION["id"] = $row['id'];
+        $_SESSION["first_name"] = $row['first_name'];
+        $_SESSION["last_name"] = $row['last_name'];
         if ($row['type'] == "Startup") {
             header("Location: startupDashboard.php");
         } else if ($row['type'] == "Business Professional") {
